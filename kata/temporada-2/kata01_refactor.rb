531ea1394d2d5a09e985df46ca2012c257b506dc
Salvatore Maria Italiano <astr0surf3r@gmail.com>
2024-10-03 12:27:31 -0700

    add the first version of the solution

---
+++ kata/temporada-2/kata01_refactor.rb
@@ -0,0 +1,23 @@
+puts "Submit a sequence of words separated by spaces and I try to retrieve the words with more than # as prefix: "
+words = gets.chomp
+
+#check if the input is empty
+if words.empty?
+  puts "You didn't submit any word/s"
+elsif words.include?("#")
+  puts "I found the following word/s with #"
+  words = words.split(" ")
+
+  #create an array with the words that contain alphabetical characters
+  words_with_alphabetical_chars = words.select { |word| word.match?(/\p{Alpha}/) }
+  puts "words with alphabetical chars are #{words_with_alphabetical_chars}"
+
+  #create an array with the words that contain #"
+  words_with_pound_prefix = words_with_alphabetical_chars.map { |word| word.chars[1] == word.chars[0] ? [word.gsub("#", "")] : [] }.compact
+  sleep 1
+  puts "#the words that contain # as prefix are the following #{words_with_pound_prefix}"
+else
+  puts "The word/s don't contain any # you submit the following word/s: "
+  sleep 1
+  puts words
+end
