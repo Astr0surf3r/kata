84ae12fe5540048e148ea8308f46f8435e419ada
Salvatore Maria Italiano <astr0surf3r@gmail.com>
2024-10-09 16:52:01 -0700

    add script files in ruby that convert Roman numeral number in numeric decimal integer

---
+++ kata/temporada-2/kata02_refactor.rb
@@ -0,0 +1,41 @@
+# Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer.
+# https://www.codewars.com/kata/51b6249c4612257ac0000005
+def mapping_roman_numbers(roman)
+  roman_numbers_values = {
+    'I' => 1,
+    'V' => 5,
+    'X' => 10,
+    'L' => 50,
+    'C' => 100,
+    'D' => 500,
+    'M' => 1000
+  }
+
+  total_of_mapping_chars = 0
+  total_substract = 0
+
+  roman.chars.each do |char|
+    total_of_mapping_chars += roman_numbers_values[char] if roman_numbers_values.include?(char)
+  end
+
+  roman.chars.each_with_index do |char, index|
+    next if index == roman.chars.size - 1
+    if roman_numbers_values[char] < roman_numbers_values[roman.chars[index + 1]] && roman_numbers_values[char] < (roman_numbers_values[roman.chars[index + 1]]/10)
+      return "Invalid roman number"
+    elsif ["I", "X", "C", "M"].include?(char) && roman.chars.count(char) > 3
+      return "Invalid roman number"
+    elsif ["V", "L", "D"].include?(char) && roman.chars.count(char) > 1
+      return "Invalid roman number"
+    elsif roman_numbers_values[char] < roman_numbers_values[roman.chars[index + 1]] && ["I", "X", "C"].include?(char)
+      total_substract += roman_numbers_values[char] 
+    end
+  end
+
+  total = total_of_mapping_chars - total_substract * 2
+end
+
+puts "Enter a roman number: "
+
+roman = gets.chomp
+
+puts "The roman number #{roman} is #{mapping_roman_numbers(roman)}"
